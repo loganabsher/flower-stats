c550a6927d14d1a04b8ca15f8c881818665ce13c
Logan Absher <absherlogan@gmail.com>
2017-05-01 12:23:49 -0700

    inital files setup

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 'use strict';
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.static('./public'));
+
+app.listen(PORT, function(){
+  console.log(`being hosted on ${PORT}`);
+});
